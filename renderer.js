5d79cdce396982aaefb014f1ac6abc19358e85c1
asantaballa <al@technowati.com>
2016-09-07 06:13:51 -0700

    First commit with seiries

---
+++ renderer.js
@@ -0,0 +1,13 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+var limit = 10;
+var result = '';
+var resultDelimiter = '<br />';
+
+for (var period = 1 ; period <= limit; period++)
+{
+    result = result.concat(period).concat(resultDelimiter); 
+}
+document.getElementById("divResult").innerHTML = result
