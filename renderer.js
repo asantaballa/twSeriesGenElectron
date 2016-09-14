a7544eabfd38afb7fb19995ea085c1ea43bfc798
asantaballa <al@technowati.com>
2016-09-14 08:17:07 -0700

    Push series changes

--- renderer.js
+++ renderer.js
@@ -2,6 +2,8 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+var d3 = require('d3');
+
 var limit = 10;
 var result = '';
 var resultDelimiter = '<br />';
